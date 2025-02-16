0abdbffb27b54269acccb1aa64388d725c06b076
Md.Emon <sheikhmdemon540@gmail.com>
2025-02-16 19:53:46 +0600

    Complete Hero section with responsive work

--- app/Pages/Home/Hero.js
+++ app/Pages/Home/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                 <h1 className="font-serif text-3xl md:text-6xl text-center mx-auto pt-5">Discover the latest trends, unbeatable deals, and exclusive collections—all in one place</h1>
                 <Link href="/Pages/Product" className="bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline focus:ring-pink-200  text-white font-semibold py-4 px-9 rounded-full mt-11 mx-auto hover:bg-blue-700 transition-all duration-300">Shop Now</Link>
             </div>
-            <Image src={hero} alt="Hero image" className="mx-auto " />
+            <Image src={hero} alt="Hero image" className="mx-auto w-full h-full md:h-3/2 md:w-2/3" />
         </div>
     );
 };
