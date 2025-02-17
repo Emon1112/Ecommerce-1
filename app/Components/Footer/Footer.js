784e4b2be2fc1d75b77f26e1dbb7ff9fbc499a46
Md.Emon <sheikhmdemon540@gmail.com>
2025-02-18 01:05:25 +0600

    I think Footer is complete

---
+++ app/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div >
+                <h1 className="text-center p-5 bg-slate-300 font-bold rounded-lg mx-5">&copy; <Link href="https://github.com/Emon1112" target="_blanc">Md. Emon Sheikh</Link> ,All right reserved</h1>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Footer;
